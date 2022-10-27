0d3afa1eb15492c60675199ce6c63fa6c21d4554
alexgicimo <gakurualexgicimo@gmail.com>
2022-10-27 19:00:20 +0300

    commit tree 06141fc8d429685cf405e9b633b82e4f9e694ccc

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,61 @@
 class Triangle
-  # write code here
+  attr_accessor :length , :base , :height
+
+  def initialize length , base , height
+      sides=[length,base,height].sort
+    
+      if [length,base,height].any? {|x| x <= 0}
+
+        raise TriangleError
+      end
+        unless sides[0]+sides[1] > sides[2]
+            
+        raise TriangleError
+        end
+      
+       @length =length
+
+       @base = base
+
+       @height = height
+      
+      
+  
+
 end
+
+def kind
+  
+  type = if (self.length == self.base && self.length == self.height)
+  
+    :equilateral
+ 
+ 
+   else if  (self.length == self.base || self.length == self.height || self.base == self.height)
+   :isosceles
+
+   else 
+  :scalene
+  end
+
+  end
+  
+ end
+
+
+  class TriangleError < StandardError
+
+  end
+end
+
+triangle1=Triangle.new(5,5,6)
+puts triangle1.kind
+
+triangle2=Triangle.new(2,2,2)
+puts triangle2.kind
+
+triangle3=Triangle.new(3,4,5)
+puts triangle3.kind
+
+triangle4=Triangle.new(1,2,2)
+puts triangle4.kind
